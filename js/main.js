603f95ae8c5c516e53586eb13c5b5f15d6dcacde
Alexander Rostyagai <136843641+Rostyagai@users.noreply.github.com>
2023-12-23 14:26:16 +0200

    Code refactor, filter, new design, scroll

--- js/main.js
+++ js/main.js
@@ -1,644 +1,769 @@
 let goods = [
   {
-    name: "T-short",
+    name: "T-shirt",
     size: "M",
     price: 100,
     color: "white",
+    img: "./images/tshirt.jpg",
   },
   {
     name: "Jeans",
     size: "S",
     price: 400,
     color: "grey",
+    img: "./images/jeans.jpg",
   },
   {
     name: "Jacket",
     size: "L",
     price: 250,
     color: "black",
+    img: "./images/jacket.jpg",
   },
   {
     name: "Skirt",
     size: "XS",
     price: 300,
     color: "red",
+    img: "./images/skirt.jpg",
   },
   {
-    name: "T-short",
+    name: "T-shirt",
     size: "M",
     price: 100,
     color: "white",
+    img: "./images/tshirt.jpg",
   },
   {
     name: "Jeans",
     size: "S",
     price: 400,
     color: "grey",
+    img: "./images/jeans.jpg",
   },
   {
     name: "Jacket",
     size: "L",
     price: 250,
     color: "black",
+    img: "./images/jacket.jpg",
   },
   {
     name: "Skirt",
     size: "XS",
     price: 300,
     color: "red",
+    img: "./images/skirt.jpg",
   },
   {
-    name: "T-short",
+    name: "T-shirt",
     size: "M",
     price: 100,
     color: "white",
+    img: "./images/tshirt.jpg",
   },
   {
     name: "Jeans",
     size: "S",
     price: 400,
     color: "grey",
+    img: "./images/jeans.jpg",
   },
   {
     name: "Jacket",
     size: "L",
     price: 250,
     color: "black",
+    img: "./images/jacket.jpg",
   },
   {
     name: "Skirt",
     size: "XS",
     price: 300,
     color: "red",
+    img: "./images/skirt.jpg",
   },
   {
-    name: "T-short",
+    name: "T-shirt",
     size: "M",
     price: 100,
     color: "white",
+    img: "./images/tshirt.jpg",
   },
   {
     name: "Jeans",
     size: "S",
     price: 400,
     color: "grey",
+    img: "./images/jeans.jpg",
   },
   {
     name: "Jacket",
     size: "L",
     price: 250,
     color: "black",
+    img: "./images/jacket.jpg",
   },
   {
     name: "Skirt",
     size: "XS",
     price: 300,
     color: "red",
+    img: "./images/skirt.jpg",
   },
   {
-    name: "T-short",
+    name: "T-shirt",
     size: "M",
     price: 100,
     color: "white",
+    img: "./images/tshirt.jpg",
   },
   {
     name: "Jeans",
     size: "S",
     price: 400,
     color: "grey",
+    img: "./images/jeans.jpg",
   },
   {
     name: "Jacket",
     size: "L",
     price: 250,
     color: "black",
+    img: "./images/jacket.jpg",
   },
   {
     name: "Skirt",
     size: "XS",
     price: 300,
     color: "red",
+    img: "./images/skirt.jpg",
   },
   {
-    name: "T-short",
+    name: "T-shirt",
     size: "M",
     price: 100,
     color: "white",
+    img: "./images/tshirt.jpg",
   },
   {
     name: "Jeans",
     size: "S",
     price: 400,
     color: "grey",
+    img: "./images/jeans.jpg",
   },
   {
     name: "Jacket",
     size: "L",
     price: 250,
     color: "black",
+    img: "./images/jacket.jpg",
   },
   {
     name: "Skirt",
     size: "XS",
     price: 300,
     color: "red",
+    img: "./images/skirt.jpg",
   },
   {
-    name: "T-short",
+    name: "T-shirt",
     size: "M",
     price: 100,
     color: "white",
+    img: "./images/tshirt.jpg",
   },
   {
     name: "Jeans",
     size: "S",
     price: 400,
     color: "grey",
+    img: "./images/jeans.jpg",
   },
   {
     name: "Jacket",
     size: "L",
     price: 250,
     color: "black",
+    img: "./images/jacket.jpg",
   },
   {
     name: "Skirt",
     size: "XS",
     price: 300,
     color: "red",
+    img: "./images/skirt.jpg",
   },
   {
-    name: "T-short",
+    name: "T-shirt",
     size: "M",
     price: 100,
     color: "white",
+    img: "./images/tshirt.jpg",
   },
   {
     name: "Jeans",
     size: "S",
     price: 400,
     color: "grey",
+    img: "./images/jeans.jpg",
   },
   {
     name: "Jacket",
     size: "L",
     price: 250,
     color: "black",
+    img: "./images/jacket.jpg",
   },
   {
     name: "Skirt",
     size: "XS",
     price: 300,
     color: "red",
+    img: "./images/skirt.jpg",
   },
   {
-    name: "T-short",
+    name: "T-shirt",
     size: "M",
     price: 100,
     color: "white",
+    img: "./images/tshirt.jpg",
   },
   {
     name: "Jeans",
     size: "S",
     price: 400,
     color: "grey",
+    img: "./images/jeans.jpg",
   },
   {
     name: "Jacket",
     size: "L",
     price: 250,
     color: "black",
+    img: "./images/jacket.jpg",
   },
   {
     name: "Skirt",
     size: "XS",
     price: 300,
     color: "red",
+    img: "./images/skirt.jpg",
   },
   {
-    name: "T-short",
+    name: "T-shirt",
     size: "M",
     price: 100,
     color: "white",
+    img: "./images/tshirt.jpg",
   },
   {
     name: "Jeans",
     size: "S",
     price: 400,
     color: "grey",
+    img: "./images/jeans.jpg",
   },
   {
     name: "Jacket",
     size: "L",
     price: 250,
     color: "black",
+    img: "./images/jacket.jpg",
   },
   {
     name: "Skirt",
     size: "XS",
     price: 300,
     color: "red",
+    img: "./images/skirt.jpg",
   },
   {
-    name: "T-short",
+    name: "T-shirt",
     size: "M",
     price: 100,
     color: "white",
+    img: "./images/tshirt.jpg",
   },
   {
     name: "Jeans",
     size: "S",
     price: 400,
     color: "grey",
+    img: "./images/jeans.jpg",
   },
   {
     name: "Jacket",
     size: "L",
     price: 250,
     color: "black",
+    img: "./images/jacket.jpg",
   },
   {
     name: "Skirt",
     size: "XS",
     price: 300,
     color: "red",
+    img: "./images/skirt.jpg",
   },
   {
-    name: "T-short",
+    name: "T-shirt",
     size: "M",
     price: 100,
     color: "white",
+    img: "./images/tshirt.jpg",
   },
   {
     name: "Jeans",
     size: "S",
     price: 400,
     color: "grey",
+    img: "./images/jeans.jpg",
   },
   {
     name: "Jacket",
     size: "L",
     price: 250,
     color: "black",
+    img: "./images/jacket.jpg",
   },
   {
     name: "Skirt",
     size: "XS",
     price: 300,
     color: "red",
+    img: "./images/skirt.jpg",
   },
   {
-    name: "T-short",
+    name: "T-shirt",
     size: "M",
     price: 100,
     color: "white",
+    img: "./images/tshirt.jpg",
   },
   {
     name: "Jeans",
     size: "S",
     price: 400,
     color: "grey",
+    img: "./images/jeans.jpg",
   },
   {
     name: "Jacket",
     size: "L",
     price: 250,
     color: "black",
+    img: "./images/jacket.jpg",
   },
   {
     name: "Skirt",
     size: "XS",
     price: 300,
     color: "red",
+    img: "./images/skirt.jpg",
   },
   {
-    name: "T-short",
+    name: "T-shirt",
     size: "M",
     price: 100,
     color: "white",
+    img: "./images/tshirt.jpg",
   },
   {
     name: "Jeans",
     size: "S",
     price: 400,
     color: "grey",
+    img: "./images/jeans.jpg",
   },
   {
     name: "Jacket",
     size: "L",
     price: 250,
     color: "black",
+    img: "./images/jacket.jpg",
   },
   {
     name: "Skirt",
     size: "XS",
     price: 300,
     color: "red",
+    img: "./images/skirt.jpg",
   },
   {
-    name: "T-short",
+    name: "T-shirt",
     size: "M",
     price: 100,
     color: "white",
+    img: "./images/tshirt.jpg",
   },
   {
     name: "Jeans",
     size: "S",
     price: 400,
     color: "grey",
+    img: "./images/jeans.jpg",
   },
   {
     name: "Jacket",
     size: "L",
     price: 250,
     color: "black",
+    img: "./images/jacket.jpg",
   },
   {
     name: "Skirt",
     size: "XS",
     price: 300,
     color: "red",
+    img: "./images/skirt.jpg",
   },
   {
-    name: "T-short",
+    name: "T-shirt",
     size: "M",
     price: 100,
     color: "white",
+    img: "./images/tshirt.jpg",
   },
   {
     name: "Jeans",
     size: "S",
     price: 400,
     color: "grey",
+    img: "./images/jeans.jpg",
   },
   {
     name: "Jacket",
     size: "L",
     price: 250,
     color: "black",
+    img: "./images/jacket.jpg",
   },
   {
     name: "Skirt",
     size: "XS",
     price: 300,
     color: "red",
+    img: "./images/skirt.jpg",
   },
   {
-    name: "T-short",
+    name: "T-shirt",
     size: "M",
     price: 100,
     color: "white",
+    img: "./images/tshirt.jpg",
   },
   {
     name: "Jeans",
     size: "S",
     price: 400,
     color: "grey",
+    img: "./images/jeans.jpg",
   },
   {
     name: "Jacket",
     size: "L",
     price: 250,
     color: "black",
+    img: "./images/jacket.jpg",
   },
   {
     name: "Skirt",
     size: "XS",
     price: 300,
     color: "red",
+    img: "./images/skirt.jpg",
   },
   {
-    name: "T-short",
+    name: "T-shirt",
     size: "M",
     price: 100,
     color: "white",
+    img: "./images/tshirt.jpg",
   },
   {
     name: "Jeans",
     size: "S",
     price: 400,
     color: "grey",
+    img: "./images/jeans.jpg",
   },
   {
     name: "Jacket",
     size: "L",
     price: 250,
     color: "black",
+    img: "./images/jacket.jpg",
   },
   {
     name: "Skirt",
     size: "XS",
     price: 300,
     color: "red",
+    img: "./images/skirt.jpg",
   },
   {
-    name: "T-short",
+    name: "T-shirt",
     size: "M",
     price: 100,
     color: "white",
+    img: "./images/tshirt.jpg",
   },
   {
     name: "Jeans",
     size: "S",
     price: 400,
     color: "grey",
+    img: "./images/jeans.jpg",
   },
   {
     name: "Jacket",
     size: "L",
     price: 250,
     color: "black",
+    img: "./images/jacket.jpg",
   },
   {
     name: "Skirt",
     size: "XS",
     price: 300,
     color: "red",
+    img: "./images/skirt.jpg",
   },
   {
-    name: "T-short",
+    name: "T-shirt",
     size: "M",
     price: 100,
     color: "white",
+    img: "./images/tshirt.jpg",
   },
   {
     name: "Jeans",
     size: "S",
     price: 400,
     color: "grey",
+    img: "./images/jeans.jpg",
   },
   {
     name: "Jacket",
     size: "L",
     price: 250,
     color: "black",
+    img: "./images/jacket.jpg",
   },
   {
     name: "Skirt",
     size: "XS",
     price: 300,
     color: "red",
+    img: "./images/skirt.jpg",
   },
   {
-    name: "T-short",
+    name: "T-shirt",
     size: "M",
     price: 100,
     color: "white",
+    img: "./images/tshirt.jpg",
   },
   {
     name: "Jeans",
     size: "S",
     price: 400,
     color: "grey",
+    img: "./images/jeans.jpg",
   },
   {
     name: "Jacket",
     size: "L",
     price: 250,
     color: "black",
+    img: "./images/jacket.jpg",
   },
   {
     name: "Skirt",
     size: "XS",
     price: 300,
     color: "red",
+    img: "./images/skirt.jpg",
   },
   {
-    name: "T-short",
+    name: "T-shirt",
     size: "M",
     price: 100,
     color: "white",
+    img: "./images/tshirt.jpg",
   },
   {
     name: "Jeans",
     size: "S",
     price: 400,
     color: "grey",
+    img: "./images/jeans.jpg",
   },
   {
     name: "Jacket",
     size: "L",
     price: 250,
     color: "black",
+    img: "./images/jacket.jpg",
   },
   {
     name: "Skirt",
     size: "XS",
     price: 300,
     color: "red",
+    img: "./images/skirt.jpg",
   },
   {
-    name: "T-short",
+    name: "T-shirt",
     size: "M",
     price: 100,
     color: "white",
+    img: "./images/tshirt.jpg",
   },
   {
     name: "Jeans",
     size: "S",
     price: 400,
     color: "grey",
+    img: "./images/jeans.jpg",
   },
   {
     name: "Jacket",
     size: "L",
     price: 250,
     color: "black",
-  },
-  {
-    name: "Skirt",
-    size: "XS",
-    price: 300,
-    color: "red",
-  },
-  {
-    name: "T-short",
-    size: "M",
-    price: 100,
-    color: "white",
-  },
-  {
-    name: "Jeans",
-    size: "S",
-    price: 400,
-    color: "grey",
-  },
-  {
-    name: "Jacket",
-    size: "L",
-    price: 250,
-    color: "black",
-  },
-  {
-    name: "Skirt",
-    size: "XS",
-    price: 300,
-    color: "red",
-  },
-  {
-    name: "T-short",
-    size: "M",
-    price: 100,
-    color: "white",
-  },
-  {
-    name: "Jeans",
-    size: "S",
-    price: 400,
-    color: "grey",
-  },
-  {
-    name: "Jacket",
-    size: "L",
-    price: 250,
-    color: "black",
-  },
-  {
-    name: "Skirt",
-    size: "XS",
-    price: 300,
-    color: "red",
-  },
-  {
-    name: "T-short",
-    size: "M",
-    price: 100,
-    color: "white",
-  },
-  {
-    name: "Jeans",
-    size: "S",
-    price: 400,
-    color: "grey",
-  },
-  {
-    name: "Jacket",
-    size: "L",
-    price: 250,
-    color: "black",
-  },
-  {
-    name: "Skirt",
-    size: "XS",
-    price: 300,
-    color: "red",
+    img: "./images/jacket.jpg",
   },
 ];
 
-const catalog = document.getElementById("catalog");
-for (item of goods) {
-  const itemElement = document.createElement("li");
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
 
-  const name = document.createElement("p");
-  const size = document.createElement("p");
-  const price = document.createElement("p");
-  const color = document.createElement("p");
+  // While there remain elements to shuffle.
+  while (currentIndex > 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
 
-  name.innerText = item.name;
-  size.innerText = item.size;
-  price.innerText = item.price;
-  color.innerText = item.color;
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+  }
 
-  itemElement.append(name, size, price, color);
-  catalog.appendChild(itemElement);
+  return array;
 }
+
+shuffle(goods);
+
+const catalog = document.getElementById("catalog");
+// for (item of goods) {
+//   const itemElement = document.createElement("li");
+
+//   const name = document.createElement("p");
+//   const size = document.createElement("p");
+//   const price = document.createElement("p");
+//   const color = document.createElement("p");
+
+//   name.innerText = item.name;
+//   size.innerText = item.size;
+//   price.innerText = item.price;
+//   color.innerText = item.color;
+
+//   itemElement.append(name, size, price, color);
+//   catalog.appendChild(itemElement);
+// }
+
+const FilterTshirts = () => {
+  tShirts = goods.filter((item) => item.name === "T-shirt");
+  let items = "";
+  for (item of tShirts) {
+    items += `<li>
+    <img src="${item.img}">
+    <p>${item.name}</p>
+    <p>${item.size}</p>
+    <p>${item.price}</p>
+    <p>${item.color}</p>
+    
+    <button class="add-to-cart-btn">Add to cart</button></li>`;
+  }
+  catalog.innerHTML = items;
+  ScrollReveal().reveal("li");
+};
+
+const FilterJackets = () => {
+  jackets = goods.filter((item) => item.name === "Jacket");
+  let items = "";
+  for (item of jackets) {
+    items += `<li>
+    <img src="${item.img}">
+    <p>${item.name}</p>
+    <p>${item.size}</p>
+    <p>${item.price}</p>
+    <p>${item.color}</p>
+    <button class="add-to-cart-btn">Add to cart</button></li>`;
+  }
+  catalog.innerHTML = items;
+  ScrollReveal().reveal("li");
+};
+const FilterAll = () => {
+  let items = "";
+  for (item of goods) {
+    items += `<li>
+    <img src="${item.img}">
+    <p>${item.name}</p>
+    <p>${item.size}</p>
+    <p>${item.price}</p>
+    <p>${item.color}</p>
+    <button class="add-to-cart-btn">Add to cart</button></li>`;
+  }
+  shuffle(goods);
+  catalog.innerHTML = items;
+  ScrollReveal().reveal("li");
+};
+
+const FilterJeans = () => {
+  jeans = goods.filter((item) => item.name === "Jeans");
+  let items = "";
+  for (item of jeans) {
+    items += `<li>
+    <img src="${item.img}">
+    <p>${item.name}</p>
+    <p>${item.size}</p>
+    <p>${item.price}</p>
+    <p>${item.color}</p>
+    <button class="add-to-cart-btn">Add to cart</button></li>`;
+  }
+  catalog.innerHTML = items;
+  ScrollReveal().reveal("li");
+};
+
+const FilterSkirts = () => {
+  skirts = goods.filter((item) => item.name === "Skirt");
+  let items = "";
+  for (item of skirts) {
+    items += `<li>
+    <img src="${item.img}">
+    <p>${item.name}</p>
+    <p>${item.size}</p>
+    <p>${item.price}</p>
+    <p>${item.color}</p>
+    <button class="add-to-cart-btn">Add to cart</button></li>`;
+  }
+  catalog.innerHTML = items;
+  ScrollReveal().reveal("li");
+};
+
+ScrollReveal({
+  delay: 150,
+  duration: 1500,
+  distance: "100px",
+  rotate: {
+    x: 0,
+    y: 60,
+    z: 0,
+  },
+});
+
+FilterAll();
