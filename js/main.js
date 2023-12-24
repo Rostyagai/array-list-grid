8a8760b8b4bbd6cf36edc8f4e679130882ef0fec
Alexander Rostyagai <136843641+Rostyagai@users.noreply.github.com>
2023-12-24 20:57:51 +0200

    Add SAAS, favicon, footer

--- js/main.js
+++ js/main.js
@@ -641,14 +641,11 @@ let goods = [
 function shuffle(array) {
   let currentIndex = array.length,
     randomIndex;
-
-  // While there remain elements to shuffle.
+  
   while (currentIndex > 0) {
-    // Pick a remaining element.
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
 
-    // And swap it with the current element.
     [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
   }
 
